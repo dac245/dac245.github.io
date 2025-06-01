a2632be3d78749ae83385514206c7ca7b1fd7d36
David Camacho <dac6040@gmail.com>
2025-06-01 02:37:18 -0400

    Changed workflows and made map usable

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ window.onload=function(){
 }
 
 function mapInfo() {
-  maptilersdk.config.apiKey = config.MAPTILER_API_KEY;
+  if (typeof config === 'undefined') {
+    maptilersdk.config.apiKey = 'ztimKDaQE7hg6qlHVOhj'
+  } else {
+    maptilersdk.config.apiKey = config.MAPTILER_API_KEY
+  }
+
   const map = new maptilersdk.Map({
     container: 'map', // container's id or the HTML element to render the map
     style: maptilersdk.MapStyle.OUTDOOR,
@@ -20,6 +25,7 @@ function modeChange() {
   const introSection = document.getElementById("intro");
   const projectsSection = document.getElementById("projects");
   const randomSection = document.getElementById("randomPics");
+  const mapSection = document.getElementById("mapSection");
 
   if (modeChanged) {    
     mainBody.style.backgroundColor = "#41444B";
@@ -32,6 +38,9 @@ function modeChange() {
 
     randomSection.style.backgroundColor = "#8586a6";
     // randomSection.style.color = "";
+
+    mapSection.style.backgroundColor = "#41444B";
+    // projectSection.style.color = "";
   } else {
     const mainBody = document.getElementById("main");
     mainBody.style.backgroundColor = "#f8f8f8";
@@ -44,5 +53,8 @@ function modeChange() {
 
     randomSection.style.backgroundColor = "#d5d6ea";
     // randomSection.style.color = "";
+
+    mapSection.style.backgroundColor = "#f8f8f8";
+    // projectSection.style.color = "";
   }
 }
